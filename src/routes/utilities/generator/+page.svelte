bb0647125104f34418c26105263102b62038eff6
vaporii <phi548182@gmail.com>
2025-02-19 09:50:48 -0600

    add units to file generator

--- src/routes/utilities/generator/+page.svelte
+++ src/routes/utilities/generator/+page.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
-	import { roundToDecimal } from '$lib';
+	import { roundToDecimal, tryRemoveFileEntry } from '$lib';
 	import Module from '../../../components/Module.svelte';
-	let size = $state(0);
+	let fileSize = $state(0);
+	let unit = $state(1);
 	let progress = $state('generate file');
 
 	async function generateFile() {
+		const size = fileSize * unit;
 		const root = await navigator.storage.getDirectory();
+		await tryRemoveFileEntry(root, 'file_v8p.me');
+
 		const draftHandle = await root.getFileHandle('file_v8p.me', { create: true });
 		const writable = await draftHandle.createWritable();
 
 		let complete = 0;
-		const chunkSize = 64 * 1024; // 64 KiB
+		const chunkSize = 64 * 1024; // 64 KiB (max crypto.getRandomValues size)
 		const stream = new ReadableStream({
 			async pull(controller) {
 				controller.enqueue(
@@ -31,6 +35,7 @@
 		const url = URL.createObjectURL(file);
 		const a = document.createElement('a');
 		a.href = url;
+		a.download = 'file.bin';
 		document.body.appendChild(a);
 		a.click();
 		a.remove();
@@ -39,13 +44,30 @@
 
 <div class="center">
 	<Module text="file generator">
-		<input type="number" bind:value={size} placeholder="bytes" />
+		<div class="wrapper">
+			<input type="number" bind:value={fileSize} />
+			<select name="sizes" id="sizes" bind:value={unit}>
+				<option value="1">bytes</option>
+				<option value="1000">kilobytes</option>
+				<option value="1000000">megabytes</option>
+				<option value="1000000000">gigabytes</option>
+			</select>
+		</div>
 		<button onclick={generateFile} class="upload">{progress}</button>
 	</Module>
 </div>
 
 <style lang="scss">
 	@use '/src/vars' as *;
+
+	.wrapper {
+		display: flex;
+		gap: $padding;
+	}
+
+	select {
+		height: 33px;
+	}
 
 	button {
 		margin-top: $padding;
