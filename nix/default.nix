f1c761ae082ad5a0f0ea360270bf70b62eac657c
vaporii <phi548182@gmail.com>
2025-04-23 10:45:32 -0500

    nix

--- nix/default.nix
+++ nix/default.nix
@@ -1,72 +1,19 @@
 { pkgs ? (import <nixpkgs> {}), ... }: let
-  # nodeDocker = pkgs.dockerTools.pullImage {
-  #   imageName = "node";
-  #   imageDigest = "";
-  #   hash = "";
-  #   finalImageTag = "22.14.0";
-  #   finalImageName = "node";
-  # };
-  # v8pPkg = pkgs.stdenv.mkDerivation {
-  #   name = "v8p.me";
-  #   version = "0.1.0";
-  #   src = ../.;
-  #   buildInputs = [ pkgs.nodejs_22 ];
-  #   buildPhase = ''
-  #     runHook preBuild
-
-  #     echo "hello!"
-  #     ls
-  #     pwd
-  #     npm install
-  #     npm run build
-  #     npm ci --omit dev
-
-  #     runHook postBuild
-  #   '';
-  #   installPhase = ''
-  #     runHook preInstall
-
-  #     mkdir -p $out/bin
-
-  #     cp package.json $out/package.json
-  #     cp package-lock.json $out/package-lock.json
-  #     cp -r build $out/build
-
-  #     cp build/index.js $out/bin/server
-  #     chmod a+x $out/bin/server
-
-  #     runHook postInstall
-  #   '';
-  # };
-  v8pPkg = pkgs.stdenv.mkDerivation {
-    name = "v8p.me";
+  v8pPkg = pkgs.buildNpmPackage {
+    pname = "v8p.me";
     version = "0.0.1";
     src = ../.;
 
-    buildInputs = [ pkgs.nodejs ];
+    npmDeps = pkgs.importNpmLock {
+      npmRoot = ../.;
+    };
 
-    buildPhase = ''
-      runHook preBuild
-
-      npm i
-      npm build
-
-      runHook postBuild
-    '';
+    npmConfigHook = pkgs.importNpmLock.npmConfigHook;
 
     installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/bin
-      cp -r build $out/bin
-
-      cp package.json $out/bin
-      cp package-lock.json $out/bin
-
-      cd $out/bin
-      npm ci --omit dev
-
-      runHook postInstall
+      mkdir -p $out/bin/v8p.me
+      cp -r build $out/bin/v8p.me
+      cp -r node_modules $out/bin/v8p.me
     '';
   };
 in
@@ -74,11 +21,15 @@ pkgs.dockerTools.buildImage {
   name = "v8p.me";
   tag = "latest";
 
-  # fromImage = nodeDocker;
-
   config = {
-    Cmd = [ "${pkgs.nodejs_22}/bin/node" "${v8pPkg}/bin/build" ];
+    Cmd = [ "/bin/node" "/bin/v8p.me/build" ];
   };
-  
-  copyToRoot = [ pkgs.nodejs_22 v8pPkg ];
+
+  copyToRoot = pkgs.buildEnv {
+    name = "v8p.me";
+    paths = [ v8pPkg pkgs.nodejs_22 ];
+    pathsToLink = [ "/bin" "/lib" ];
+  };
+
+  keepContentsDirlinks = true;
 }
