29a0abec7f5955153a3cba98a8284a1b872a2650
vaporii <phi548182@gmail.com>
2025-04-23 11:22:59 -0500

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
+import packageJson from "./package.json";
+
+const dependencies = Object.keys({
+  ...packageJson.dependencies,
+  ...packageJson.devDependencies
+});
+
+const noExternal = dependencies;
+
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  ssr: {
+    noExternal
+  }
 });
