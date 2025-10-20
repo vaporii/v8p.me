0a2f8528f46a0729b6a713797f27c23b40940d5e
vaporii <phi548182@gmail.com>
2025-10-20 00:16:48 -0500

    feat: add formatting and filesize to zip display

--- src/components/file_displays/Zip.svelte
+++ src/components/file_displays/Zip.svelte
@@ -1,24 +1,47 @@
 <script lang="ts">
+  import { formatSize } from "$lib";
   import { ZipReader, HttpReader, BlobReader, HttpRangeReader } from "@zip.js/zip.js";
 
   let { url }: { url: string } = $props();
 
   async function getZipResults(url: string) {
     if (url.startsWith("blob:")) {
-      const req = await fetch(url)
+      const req = await fetch(url);
       return new ZipReader(new BlobReader(await req.blob())).getEntries();
     } else {
       return new ZipReader(new HttpRangeReader(url, {})).getEntries();
     }
   }
-
-  // console.log(getZipResults(blob));
 </script>
 
-{#await getZipResults(url)}
-  loading...
-{:then results}
-  {#each results as result}
-    <div>{result.filename}</div>
-  {/each}
-{/await}
+<div class="wrapper">
+  {#await getZipResults(url)}
+    loading...
+  {:then results}
+    <div id="file-table">
+      {#each results as result}
+        <div class="table-filename">{result.filename}</div>
+        <div class="table-filesize">{formatSize(result.uncompressedSize)}</div>
+      {/each}
+    </div>
+  {/await}
+</div>
+
+<style lang="scss">
+  #file-table {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    width: 100%;
+  }
+
+  .table-filename {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .table-filesize {
+    white-space: nowrap;
+    text-align: right;
+  }
+</style>
